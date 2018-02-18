2cea4d1b47b494017a868e32618daf54f4b0d0cc
Samy2543 <samugope@live.com.mx>
2018-02-17 22:42:48 -0600

    en esta carpeta se encuentran los archivos con terminacion js

---
+++ js/funciones.js
@@ -0,0 +1,27 @@
+function modifica (){ 
+var videlem = document.createElement("video");
+var sourceMP4 = document.createElement("source"); 
+sourceMP4.type = "video/mp4";
+sourceMP4.src = "video/video4.mp4";
+sourceMP4.controls;
+videlem.appendChild(sourceMP4);
+document.getElementById("caja2").appendChild(videlem);
+}
+function modifica2 (){ 
+var videlem = document.createElement("video");
+var sourceMP4 = document.createElement("source"); 
+sourceMP4.type = "video/mp4";
+sourceMP4.src = "video/video4.mp4";
+sourceMP4.controls;
+videlem.appendChild(sourceMP4);
+document.getElementById("caja2").appendChild(videlem);
+}
+function modifica3 (){ 
+var videlem = document.createElement("video");
+var sourceMP4 = document.createElement("source"); 
+sourceMP4.type = "video/mp4";
+sourceMP4.src = "video/video4.mp4";
+sourceMP4.controls;
+videlem.appendChild(sourceMP4);
+document.getElementById("caja2").appendChild(videlem);
+}
